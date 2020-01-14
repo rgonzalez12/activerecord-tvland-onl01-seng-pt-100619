47fdbe397a85b127acb1198fcde445722839c6f7
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-14 05:12:52 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_character.rb
+++ db/migrate/006_add_catchphrase_to_character.rb
@@ -1,1 +1,7 @@
-class AddCatchphraseToCharacter < ActiveRecord::Migration 
+class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
+  
+  def change
+    add_column :characters, :catchphrase, :string
+  end
+  
+end
