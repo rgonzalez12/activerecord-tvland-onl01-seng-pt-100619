9246073de66be49b3eb7a088a9da2a6a84871626
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-14 05:31:55 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_day_and_genre_and_season_to_shows.rb
+++ db/migrate/008_add_day_and_genre_and_season_to_shows.rb
@@ -1,4 +1,4 @@
-class AddDayAmdGenreAndSeasonToShows < ActiveRecord::Migration[5.1]
+class AddDayAndGenreAndSeasonToShows < ActiveRecord::Migration[5.1]
   
   def change
     add_column :shows, :day, :string
